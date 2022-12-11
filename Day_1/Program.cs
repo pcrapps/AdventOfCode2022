5234eec2cf63926f4bec3157c0720443f51e4640
pcrapps <patrickcrapps@gmail.com>
2022-12-11 14:37:50 -0600

    Day 1

---
+++ Day_1/Program.cs
@@ -0,0 +1,15 @@
+﻿//read in the files named elf in the elf directory
+var elfs = new Dictionary<string, int>();
+var numberofElves = 0;
+foreach (var file in Directory.GetFiles("elf"))
+{
+    numberofElves++;
+    var elfName = Path.GetFileNameWithoutExtension(file);
+    var calories = File.ReadAllLines(file).ToList();
+    var sumOfCalories = calories.Sum(x => int.Parse(x));
+    elfs.Add(elfName, sumOfCalories);
+}
+
+var topElf = elfs.OrderByDescending(x => x.Value).First();
+
+Console.WriteLine($"The top elf is {topElf.Key} with {topElf.Value} calories");
